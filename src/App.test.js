7230b4f79e299eb9063ac8e582de7356508dd425
Clousk <kenjdownie@gmail.com>
2022-06-18 19:29:09 -0400

    update app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+/*
+  Components:
+    - DM HUD
+      - Here the DM can update/view all components and update them updating them
+      - Updating components here updates across app
+    - Map
+      - Resize/rotate
+      - Toggle grid on/off
+      - Change color of grid
+      - Split view
+    - Player Charcters
+      - Playing card layout
+      - Character Picture
+      - Basic stats
+      - Notifications
+      - Inventory/currency
+      - Spells
+    - Soundboard
+      - Looping sounds
+      - Play once sounds
+      - Soundscapes
+*/
+const App = () => {
+  <>
+    <h4>Components</h4>
+    <ul>
+      <li></li>
+    </ul>
+  </>
+  
+};
+
+export default App;
